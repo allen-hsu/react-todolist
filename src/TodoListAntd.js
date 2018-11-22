02e6b66770eb4d329e050d3e77abc6dbcf11f2e0
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-11-22 23:42:15 +0800

    finish todolist delete function

--- src/TodoListAntd.js
+++ src/TodoListAntd.js
@@ -30,7 +30,11 @@ class TodoListAntd extends Component {
           style={{ marginTop: "10px", width: "300px" }}
           bordered
           dataSource={this.state.list}
-          renderItem={item => <List.Item>{item}</List.Item>}
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
+              {item}
+            </List.Item>
+          )}
         />
       </div>
     );
@@ -48,6 +52,13 @@ class TodoListAntd extends Component {
   handleBtnClick() {
     const action = {
       type: "add_todo_item"
+    };
+    store.dispatch(action);
+  }
+  handleItemDelete(index) {
+    const action = {
+      type: "delete_todo_item",
+      value: index
     };
     store.dispatch(action);
   }
