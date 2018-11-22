b15e228276e90ae48baf5ad946fbd86c193282f3
Allen Hsu <allenhsu.taiwan@gmail.com>
2018-11-22 23:28:47 +0800

    finish todolist redux

--- src/TodoListAntd.js
+++ src/TodoListAntd.js
@@ -1,34 +1,55 @@
 import React, { Component } from "react";
 import "antd/dist/antd.css";
 import { List, Input, Button } from "antd";
-
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash.",
-  "Man charged over missing wedding girl.",
-  "Los Angeles battles huge wildfires."
-];
+import store from "./store";
 
 class TodoListAntd extends Component {
+  constructor(props) {
+    super(props);
+    this.state = store.getState();
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleStoreChange = this.handleStoreChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    store.subscribe(this.handleStoreChange);
+  }
   render() {
     return (
       <div style={{ marginTop: "10px", marginLeft: "10px" }}>
         <div>
           <Input
+            value={this.state.inputValue}
             placeholder="todo info"
             style={{ width: "300px", marginRight: "10px" }}
+            onChange={this.handleInputChange}
           />
-          <Button type="primary">提交</Button>
+          <Button type="primary" onClick={this.handleBtnClick}>
+            提交
+          </Button>
         </div>
         <List
           style={{ marginTop: "10px", width: "300px" }}
           bordered
-          dataSource={data}
+          dataSource={this.state.list}
           renderItem={item => <List.Item>{item}</List.Item>}
         />
       </div>
     );
+  }
+  handleInputChange(e) {
+    const action = {
+      type: "change_input_value",
+      value: e.target.value
+    };
+    store.dispatch(action);
+  }
+  handleStoreChange() {
+    this.setState(store.getState());
+  }
+  handleBtnClick() {
+    const action = {
+      type: "add_todo_item"
+    };
+    store.dispatch(action);
   }
 }
 
